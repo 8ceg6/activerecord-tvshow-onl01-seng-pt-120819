1869d479c04a588888687b946698f3578d390d6a
cedric e goddard <8ceg6@comcast.net>
2020-03-08 02:21:36 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,4 +7,8 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
       self.where(rating: highest_rating).first
   end 
+  
+  def self.lowest_rating
+      self.where(rating: highest_rating).last
+  end 
 end 
